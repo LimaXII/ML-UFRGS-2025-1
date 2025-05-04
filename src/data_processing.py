be2c9c4a163d74c26c8fd575e25f6624a8cf1a1b
LimaXII <luccasdasilvalima@hotmail.com>
2025-05-04 16:52:07 -0300

    feat: graphs saved

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,30 +1,11 @@
-import pandas as pd
-import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
-from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
 
 
-df = pd.read_csv('../data/Clean_Dataset.csv')
-
-## 4. Identificação e Tratamento de Problemas
-
-### a) Dados Faltantes
-print("\n=== DADOS FALTANTES ===\n")
-print("Quantidade de valores nulos por coluna:")
-print(df.isnull().sum())
-
-# Visualização gráfica dos dados faltantes
-plt.figure(figsize=(10, 6))
-sns.heatmap(df.isnull(), cbar=False, cmap='viridis')
-plt.title("Mapa de Valores Nulos no Dataset")
-plt.show()
-
-
-### b) Identificação de Outliers
-print("\n=== OUTLIERS ===\n")
-
-# Função para identificar outliers
 def detect_outliers(col):
     Q1 = col.quantile(0.25)
     Q3 = col.quantile(0.75)
@@ -33,17 +14,37 @@ def detect_outliers(col):
     upper_bound = Q3 + 1.5 * IQR
     return ((col < lower_bound) | (col > upper_bound)).sum()
 
+
+matplotlib.use('Agg')
+df = pd.read_csv('data/Clean_Dataset.csv')
+
+# 4. Identificação e Tratamento de Problemas
+
+# a) Dados Faltantes
+print("\n=== DADOS FALTANTES ===\n")
+print("Quantidade de valores nulos por coluna:")
+print(df.isnull().sum())
+
+# Visualização gráfica dos dados faltantes
+plt.figure(figsize=(10, 6))
+sns.heatmap(df.isnull(), cbar=False, cmap='viridis')
+plt.title("Mapa de Valores Nulos no Dataset")
+plt.savefig("graphs/null_map.png")
+
+# b) Identificação de Outliers
+print("\n=== OUTLIERS ===\n")
+
 # Verificar outliers nas colunas numéricas
 numeric_cols = ['duration', 'days_left', 'price']
 for col in numeric_cols:
-    outliers = detect_outliers(df[col])
+    outliers = detect_outliers(col=df[col])
     print(f"{col}: {outliers} outliers")
 
 # Visualização de outliers para a variável Price
 plt.figure(figsize=(10, 6))
 sns.boxplot(x=df['price'])
 plt.title("Boxplot de Preços (Identificação de Outliers)")
-plt.show()
+plt.savefig("graphs/outliers.png")
 
 # Opções para tratar outliers:
 # 1. Remover
@@ -54,11 +55,11 @@ plt.show()
 # from scipy.stats.mstats import winsorize
 # df['Price_winsorized'] = winsorize(df['Price'], limits=[0.05, 0.05])
 
-### c) Engenharia de Features
+# c) Engenharia de Features
 print("\n=== ENGENHARIA DE FEATURES ===\n")
 
 # Converter departure_time e arrival_time para categorias temporais
-time_mapping = {
+time_mapping: dict = {
     'Early_Morning': 0,
     'Morning': 1,
     'Afternoon': 2,
@@ -84,13 +85,13 @@ df['stops_numeric'] = df['stops'].map(stop_mapping)
 df['stops_numeric'].fillna(0, inplace=True)
 
 print("\nNovas features criadas:")
-print(df[['departure_time_num', 'arrival_time_num', 'route_combined', 'stops_numeric']].head())
+print(df[['departure_time_num', 'arrival_time_num',
+      'route_combined', 'stops_numeric']].head())
 
 
-### d) Normalização
+# d) Normalização
 print("\n=== NORMALIZAÇÃO ===\n")
 
-from sklearn.preprocessing import MinMaxScaler
 
 # Verificar a necessidade de normalização
 print("Estatísticas antes da normalização:")
@@ -103,27 +104,28 @@ df['price_normalized'] = scaler.fit_transform(df[['price']])
 print("\nEstatísticas após normalização do price:")
 print(df['price_normalized'].describe())
 
-### e) Balanceamento
+# e) Balanceamento
 print("\n=== BALANCEAMENTO ===\n")
 
 # Verificar balanceamento nas colunas categóricas
-categorical_cols = ['airline', 'source_city', 'destination_city', 'stops', 'class']
+categorical_cols = ['airline', 'source_city',
+                    'destination_city', 'stops', 'class']
 for col in categorical_cols:
     print(f"\nDistribuição de {col}:")
     print(df[col].value_counts(normalize=True) * 100)
-    
+
     # Visualização
     plt.figure(figsize=(10, 5))
     sns.countplot(y=col, data=df, order=df[col].value_counts().index)
     plt.title(f'Distribuição de {col}')
-    plt.show()
+    plt.savefig(f"graphs/distribution-{col}.png")
 
 # Se houver classes muito desbalanceadas, considerar:
 # 1. Undersampling
 # 2. Oversampling (SMOTE)
 # 3. Ponderar classes no modelo
 
-### f) Tratamento de Inconsistências
+# f) Tratamento de Inconsistências
 print("\n=== INCONSISTÊNCIAS ===\n")
 
 # Verificar durações inconsistentes
@@ -133,7 +135,6 @@ print(df[df['duration'] <= 0][['source_city', 'destination_city', 'duration']])
 # Verificar preços zero ou negativos
 print("\nVoos com preço zero ou negativo:")
 print(df[df['price'] <= 0][['airline', 'source_city', 'destination_city', 'price']])
-
 
 
 # ### g) Remoção de Colunas Problemáticas
